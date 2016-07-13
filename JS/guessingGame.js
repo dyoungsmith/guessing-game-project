abba90141b8c1952c986085cc254b96694428fdc
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-07-13 11:13:53 -0400

    style update

--- JS/guessingGame.js
+++ JS/guessingGame.js
@@ -21,6 +21,30 @@ function playersGuessSubmission(){
 	
 }
 
+// Check if the Player's Guess is the winning number 
+function checkGuess(){
+	if (guessList.indexOf(playersGuess) > -1){
+		var duplicateGuess = $('<p class="message">You already guessed that!</p>');
+		$('.guess').append(duplicateGuess);
+	}
+	else {
+		guessList.push(playersGuess);
+		numGuesses += 1;
+		if (numGuesses == 6){
+			var noMoreGuesses = $('<p class="message" id="noGuesses">Sorry, you are out of guesses, play again!</p>');
+			$('.guess').append(noMoreGuesses);
+		}
+		else if (playersGuess == winningNumber){
+			var playerWins = $('<p class="message" id="win">YOU WIN!!!</p>');
+			$('.guess').append(playerWins);
+		}
+		else if (playersGuess != winningNumber){
+			var guessAgain = $('<p class="message" id="tryAgain">Try Again</p>');
+			$('.guess').append(guessAgain);
+		}
+	}		
+}
+
 // Determine if the next guess should be a lower or higher number
 function lowerOrHigher(){
 	var difference = winningNumber - playersGuess;
@@ -34,55 +58,25 @@ function guessMessage (){
 	var absDiff = Math.abs(diff);
 	var distance = 0;
 	if (absDiff == 0){
-		var message = $('<p class="message">The number was '+ playersGuess +'</p>');
+		var message = $('<p class="message" id="winNum">The number was '+ playersGuess +'</p>');
 		$('.guess').append(message);
 	}
 	else{
-		if (absDiff > 0 && absDiff <= 5){
-		distance = 5;
-		}
-		if (absDiff > 5 && absDiff <= 10){
-			distance = 10;
-		}
-		if (absDiff > 10 && absDiff <= 20){
-			distance = 20;
-		}
-		if (absDiff > 20 && absDiff <= 50){
-			distance = 50;
-		}
-		if (absDiff > 50){
-			distance = 99;
-		}
 		if (diff < 0){
 			guessDirection = 'higher';
 		}
 		if (diff > 0){
 			guessDirection = 'lower';
 		}
-		var message = $('<p class="message">Your guess is '+guessDirection+' and within '+distance+' digits of the winning number</p>');
-		$('.guess').append(message);
-	}
-	
-}
-
-// Check if the Player's Guess is the winning number 
-function checkGuess(){
-	if (guessList.indexOf(playersGuess) > -1){
-		var duplicateGuess = $('<p class="message">You already guessed that!</p>');
-		$('.guess').append(duplicateGuess);
-	}
-	else {
-		guessList.push(playersGuess);
-		numGuesses += 1;
-		if (playersGuess == winningNumber){
-			var playerWins = $('<p class="message">You Win!</p>');
-			$('.guess').append(playerWins);
+		distance = Math.round(absDiff / 10) * 10;
+		if (distance < 10){
+			distance = 5;
 		}
-		else if (playersGuess != winningNumber){
-			var guessAgain = $('<p class="message">Try Again</p>');
-			$('.guess').append(guessAgain);
+		if (numGuesses < 6){
+			var message = $('<p class="message" id="info">Your guess is '+guessDirection+' and within '+distance+' digits of the winning number</p>');
+			$('.guess').append(message);
 		}
-	}		
+	}
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
@@ -91,7 +85,6 @@ function provideHint(){
 	  var i = 0,
 	      j = 0,
 	      temp = null;
-
 	  for (i = array.length - 1; i > 0; i -= 1) {
 	    j = Math.floor(Math.random() * (i + 1))
 	    temp = array[i]
@@ -111,7 +104,7 @@ function provideHint(){
 	hintNums.push(final);
 	var shuffledNums = shuffle(hintNums);
 	
-	var hint = $('<p>' + shuffledNums.join(', ') + '</p>');
+	var hint = $('<p class="message" id="hint">' + shuffledNums.join(', ') + '</p>');
 	$('body').append(hint);
 }
 
