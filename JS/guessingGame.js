88cc8e00c433c2906d390276d26d7136ce6cc712
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-07-13 10:43:11 -0400

    Updated

--- JS/guessingGame.js
+++ JS/guessingGame.js
@@ -17,9 +17,6 @@ function generateWinningNumber(){
 // Fetch the Players Guess
 function playersGuessSubmission(){
 	playersGuess = +$('#userGuess').val();
-	if (numGuesses == 0) {
-		guessList.push(playersGuess);
-	}
 	$('#userGuess').val('')
 	
 }
@@ -36,55 +33,53 @@ function guessMessage (){
 	var diff = lowerOrHigher();
 	var absDiff = Math.abs(diff);
 	var distance = 0;
-	if (absDiff > 0 && absDiff <= 5){
+	if (absDiff == 0){
+		var message = $('<p class="message">The number was '+ playersGuess +'</p>');
+		$('.guess').append(message);
+	}
+	else{
+		if (absDiff > 0 && absDiff <= 5){
 		distance = 5;
-	}
-	if (absDiff > 5 && absDiff <= 10){
-		distance = 10;
-	}
-	if (absDiff > 10 && absDiff <= 20){
-		distance = 20;
-	}
-	if (absDiff > 20 && absDiff <= 50){
-		distance = 50;
-	}
-	if (absDiff > 50){
-		distance = 99;
-	}
-	if (diff < 0){
-		guessDirection = 'higher';
-	}
-	if (diff > 0){
-		guessDirection = 'lower';
-	}
-	var message = $('<p>Your guess is '+guessDirection+' and within '+distance+' digits of the winning number</p>');
-	$('.guess').append(message);
-}
-
-//Check if the Player's Guess is a duplicate
-function contains(itemList, targetItem){
-	for (i=0; i<itemList.length; i++){
-		if (itemList[i] === targetItem){
-			return true;
 		}
+		if (absDiff > 5 && absDiff <= 10){
+			distance = 10;
+		}
+		if (absDiff > 10 && absDiff <= 20){
+			distance = 20;
+		}
+		if (absDiff > 20 && absDiff <= 50){
+			distance = 50;
+		}
+		if (absDiff > 50){
+			distance = 99;
+		}
+		if (diff < 0){
+			guessDirection = 'higher';
+		}
+		if (diff > 0){
+			guessDirection = 'lower';
+		}
+		var message = $('<p class="message">Your guess is '+guessDirection+' and within '+distance+' digits of the winning number</p>');
+		$('.guess').append(message);
 	}
+	
 }
 
 // Check if the Player's Guess is the winning number 
 function checkGuess(){
-	if (contains(guessList, playersGuess) === true){
-		var duplicateGuess = $('<p>You already guessed that!</p>');
+	if (guessList.indexOf(playersGuess) > -1){
+		var duplicateGuess = $('<p class="message">You already guessed that!</p>');
 		$('.guess').append(duplicateGuess);
 	}
 	else {
 		guessList.push(playersGuess);
 		numGuesses += 1;
 		if (playersGuess == winningNumber){
-			var playerWins = $('<p>You Win!</p>');
+			var playerWins = $('<p class="message">You Win!</p>');
 			$('.guess').append(playerWins);
 		}
 		else if (playersGuess != winningNumber){
-			var guessAgain = $('<p>Try Again</p>');
+			var guessAgain = $('<p class="message">Try Again</p>');
 			$('.guess').append(guessAgain);
 		}
 	}		
@@ -92,12 +87,37 @@ function checkGuess(){
 
 // Create a provide hint button that provides additional clues to the "Player"
 function provideHint(){
-	// add code here
+	var shuffle = function(array){
+	  var i = 0,
+	      j = 0,
+	      temp = null;
+
+	  for (i = array.length - 1; i > 0; i -= 1) {
+	    j = Math.floor(Math.random() * (i + 1))
+	    temp = array[i]
+	    array[i] = array[j]
+	    array[j] = temp
+	  }
+	  return array;
+	}
+	
+	var hintNums = [];
+	hintNums.push(generateWinningNumber());
+	hintNums.push(winningNumber);
+	var final = generateWinningNumber();
+	while (hintNums.indexOf(final) > -1){
+		final = generateWinningNumber();
+	}
+	hintNums.push(final);
+	var shuffledNums = shuffle(hintNums);
+	
+	var hint = $('<p>' + shuffledNums.join(', ') + '</p>');
+	$('body').append(hint);
 }
 
 // Allow the "Player" to Play Again
 function playAgain(){
-	// add code here
+	//All resets taken care of in default event
 }
 
 
@@ -107,18 +127,22 @@ $(document).ready(function() {
 
 	$('.guess').on('click', '#submitGuess', function(event){
 		event.preventDefault();
+		$('.guess').remove('.message');  //REMOVE POP UP MESSAGES AT EACH CLICK
 		playersGuessSubmission();
 		checkGuess();
 		guessMessage();
-		//$('.guess').remove(duplicateGuess);
 	});
 
 	$('.buttons').on('click', '#hint', function(){
-		var hintNums = [];
-		hintNums.push(generateWinningNumber());
-		var hint = $('<p>Try Again</p>');
-		$('body').append(hint);
+		//REMOVE POP UP MESSAGE ON EACH CLICK
+		event.preventDefault();
+		provideHint();
 	});
+
+	// $('.buttons').on('click', '#playAgain', function(){
+	// 	playAgain();
+	// });
+
 });
 
 
